9854a3e3e85163f7e8499a48edde1175ffd8d35c
DerekHuang <aulimarutama@rotome.me>
2023-05-17 13:49:45 +0800

    adding code

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import pygame
-width = 1024
-height = 768
+width = 1280
+height = 720
 screen_size = (width, height)
+pygame.init()
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
+running = True
+class Ball():
+    def __init__(self,x,y,radius):
+
+while running:
+    for event in pygame.event.get(): # poll for events, pygame.QUIT event means the user clicked X to close your window
+        if event.type == pygame.QUIT:
+            running = False # if event type is pygame.QUIT, then quit
+    # RENDER YOUR GAME HERE
+
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
