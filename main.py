456fda88d19e68ae8fb6f2539f743e11974c19f3
DerekHuang <aulimarutama@rotome.me>
2023-08-03 15:17:15 +0800

    fix bug, add ac/deceleration system to ball. Todo: prettify, fix spelling, rewrite platform

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 import sys
 import math
@@ -6,21 +8,31 @@ class Ball():
     def __init__(self, x, y, length):
         self.rect = pygame.Rect(0, 0, length, length)
         self.length = length
-        self.rect.center = (x, y)
-        self.speed = 5
-        self.velocity = pygame.math.Vector2(3, 4)
-        self.accel = 0.02  # (0,1] smaller the value, slower the ac/deceleration, 1 will set speed to default always.
+        self.center = (x, y)
+        self.speed = 3
+        self.velocity = self.init_velocity()
+        self.accel = 0.02  # (0,1] smaller the value, slower the ac/deceleration, 1 will set speed to self.speed always.
         self.owner = None
 
     def vector_to_speed(self, x):
         return math.sqrt(x.x**2+x.y**2)
+
+    def init_velocity(self):
+        x = 0.13+random.random()*0.86  # to make the first ball never go towards the platform, x = 0.13 ~ 0.99
+        y = math.sqrt(1-x**2)
+        x *= (random.randint(0, 1) - 0.5) * 2
+        y *= (random.randint(0, 1) - 0.5) * 2
+        return pygame.math.Vector2(x*self.speed, y*self.speed)
+
+
     def move(self):
-        self.rect.center += self.velocity
+        self.center += self.velocity
+        self.rect.center=self.center
         self.velocity /= (self.vector_to_speed(self.velocity) / self.speed - 1) * self.accel + 1  # deceleration
 
     def render(self):
-        # pygame.draw.line(screen, "red", self.rect.center, self.velocity*999+self.rect.center)
-        # pygame.draw.line(screen, "green", self.rect.center, self.rect.center+self.velocity*10)  # the direction line and prediction line are bad because of int problem, but IDK how.
+        pygame.draw.line(screen, "red", self.rect.center, self.velocity*1024+self.rect.center)
+        pygame.draw.line(screen, "green", self.rect.center, self.rect.center+self.velocity*10)  # the direction line and prediction line are bad because of int problem, but IDK how.
         pygame.draw.rect(screen, "white", self.rect)  # draw the ball on screen
 
     def check_outside(self):
@@ -57,26 +69,37 @@ class Ball():
 
 
 class Platform():
-    def __init__(self, x, y, width, height, keymaps):
+    def __init__(self, x, y, width, height, keymaps, algorithm):
         self.rect = pygame.Rect(0, 0, width, height)
         self.rect.center = (x, y)
         self.keymaps = keymaps
         self.speed = 10
         self.score = 0
         self.direction = pygame.Vector2(0, 0)
+        self.algorithm = algorithm
 
     def render(self):
         pygame.draw.rect(screen, "white", self.rect)
         text_surface = FONT.render(f"{self.score}", True, "white")
         screen.blit(text_surface, self.rect.bottomright)
 
-    def control(self):
+    #  Algorithms
+    def manual(self):
         # sets direction when key pressed
-        self.direction = pygame.Vector2(0, 0)
+        direction = pygame.Vector2(0, 0)
         pressed_keys = pygame.key.get_pressed()
         for keymap in self.keymaps:
             if pressed_keys[keymap]:
-                self.direction += self.keymaps.get(keymap)
+                direction += self.keymaps.get(keymap)
+        return direction
+
+    def chaseball(self):
+        pass  # I can't fucking do this, so hard
+
+    def control(self):
+        match self.algorithm:
+            case "manual":
+                self.direction = self.manual()
 
     def move(self):
         # moves platform according to direction
@@ -143,7 +166,7 @@ def main():
 
 if __name__ == "__main__":
     pygame.init()
-    screen_size = (1000, 1000)
+    screen_size = (1024, 1024)
     screen = pygame.display.set_mode(screen_size)
     screen_rect = screen.get_rect()
     clock = pygame.time.Clock()
@@ -155,9 +178,9 @@ if __name__ == "__main__":
     RIGHT = pygame.Vector2(1, 0)
     GAP = 100
     platforms = [
-            Platform(screen_rect.width // 2, GAP, 100, 10, {pygame.K_a: LEFT, pygame.K_d: RIGHT}),
-            Platform(screen_rect.width // 2, screen_rect.height - GAP, 100, 10, {pygame.K_LEFT: LEFT, pygame.K_RIGHT: RIGHT}),
-            Platform(GAP, screen_rect.height // 2, 10, 100, {pygame.K_w: UP, pygame.K_s: DOWN}),
-            Platform(screen_rect.width - GAP, screen_rect.height // 2, 10, 100, {pygame.K_UP: UP, pygame.K_DOWN: DOWN}),
+            Platform(screen_rect.width // 2, GAP, 100, 10, {pygame.K_a: LEFT, pygame.K_d: RIGHT}, "manual"),
+            Platform(screen_rect.width // 2, screen_rect.height - GAP, 100, 10, {pygame.K_LEFT: LEFT, pygame.K_RIGHT: RIGHT}, "manual"),
+            Platform(GAP, screen_rect.height // 2, 10, 100, {pygame.K_w: UP, pygame.K_s: DOWN}, "manual"),
+            Platform(screen_rect.width - GAP, screen_rect.height // 2, 10, 100, {pygame.K_UP: UP, pygame.K_DOWN: DOWN}, "manual"),
     ]
     main()
